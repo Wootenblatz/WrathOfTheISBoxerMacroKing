8265210724eb33378d42419fe9a7313a9caf3609
Zach Karpinski <zkarpinski@gmail.com>
2021-05-09 18:09:25 -0500

    Moving javascript source

---
+++ generator/Data/Classes.js
@@ -0,0 +1,13 @@
+var classes;
+classes = [
+    {id: 1, name: "Warrior"},
+    {id: 2, name: "Paladin" },
+    {id: 3, name: "Hunter" },
+    {id: 4, name: "Rogue" },
+    {id: 5, name: "Priest" },
+    {id: 6, name: "Death Knight" },
+    {id: 7, name: "Shaman" },
+    {id: 8, name: "Mage" },
+    {id: 9, name: "Warlock" },
+    {id: 10, name: "Druid" }
+];
