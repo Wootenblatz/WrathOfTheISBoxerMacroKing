e0298aedfb4e99fc5bee0442ae4a8497f3b19a84
Zach Karpinski <zkarpinski@gmail.com>
2021-05-09 19:24:48 -0500

    Refactor javascript into classes and folded into main classes class.  CLASS!

--- generator/Data/Classes.js
+++ generator/Data/Classes.js
@@ -1,6 +1,29 @@
+const Warrior = require('./Warrior');
+const Paladin = require('./Paladin');
+const Hunter = require('./Hunter');
+const Rogue = require('./Rogue');
+const Priest = require('./Priest');
+const DeathKnight = require('./DeathKnight');
+const Shaman = require('./Shaman');
+const Mage = require('./Mage');
+const Warlock = require('./Warlock');
+const Druid = require('./Druid');
+
 class ClassList {
+
     getValues() {
-        return [{id: 1, name: "Warrior"}, {id: 2, name: "Paladin" }, {id: 3, name: "Hunter" }, {id: 4, name: "Rogue" }, {id: 5, name: "Priest" }, {id: 6, name: "Death Knight" }, {id: 7, name: "Shaman" }, {id: 8, name: "Mage" }, {id: 9, name: "Warlock" }, {id: 10, name: "Druid" }];
+        return [
+            {id: 1, name: "Warrior", obj: new Warrior()},
+            {id: 2, name: "Paladin", obj: new Paladin()},
+            {id: 3, name: "Hunter", obj: new Hunter()},
+            {id: 4, name: "Rogue", obj: new Rogue()},
+            {id: 5, name: "Priest", obj: new Priest()},
+            {id: 6, name: "DeathKnight", obj: new DeathKnight()},
+            {id: 7, name: "Shaman", obj: new Shaman()},
+            {id: 8, name: "Mage", obj: new Mage()},
+            {id: 9, name: "Warlock", obj: new Warlock()},
+            {id: 10, name: "Druid", obj: new Druid()}
+        ];
     }
 }
 
